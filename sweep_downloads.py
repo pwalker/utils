752df3f7d7b9c00deddd377ad0cf46cb91b563f1
Peter Walker <peterw@suprafax.net>
2013-04-06 09:33:34 -0400

    typo

--- sweep_downloads.py
+++ sweep_downloads.py
@@ -10,12 +10,12 @@ def is_old(f):
 
     # the last thing to happen to this file was 1 week ago
     last_time = datetime.datetime.fromtimestamp(max(os.stat(f).st_atime, os.stat(f).st_mtime)) 
-    
+
     return (last_time + delta) < datetime.datetime.now()
 
 def remove(f):
     subprocess.call(["trash-put",f])
-    print(f + " id old!")
+    print(f + " is old!")
 
 # process each file
 for f in os.listdir(directory):
